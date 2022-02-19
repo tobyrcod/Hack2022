f806fa77eed11d59bd626584833bf20f8a6579a6
ZacWilkes-Green <92087410+ZacWilkes-Green@users.noreply.github.com>
2022-02-19 16:17:42 +0000

    coordToPostCode
    Added JS script with the function coordToPostCode

---
+++ coordtopostcode.js
@@ -0,0 +1,18 @@
+startlat = -1.584512
+startlong = 54.775413
+
+function coordToPostCode(Lat, Long){
+    const Http = new XMLHttpRequest();
+    const url=`https://findthatpostcode.uk/points/${Long},${Lat}`
+    Http.open("GET", url);
+    Http.send();
+
+    Http.onreadystatechange=e=>{
+        JSONobj = JSON.parse(Http.responseText)
+        postcode = JSONobj.data.relationships.nearest_postcode.data.id
+        console.log(postcode)
+        return(postcode)
+    }
+}
+
+coordToPostCode(startlat, startlong)
